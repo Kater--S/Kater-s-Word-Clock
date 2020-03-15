22031f9f6455d59130816be3f50752addbdb545e
Kater_S <github.tulux@xoxy.net>
2020-03-15 10:22:40 +0100

    add MQTT (c3RE doorstatus), add config.h, eliminate unused colorconv.h

---
+++ ESP8266/WordClock_V1.5_MQTT/config.h
@@ -0,0 +1,10 @@
+#define CLOCK 1
+#define SPACEAPI 1
+
+#ifdef SPACEAPI
+const char* C_MQTTSERVER = "...";
+const char* C_MQTTUSER = "...";
+const char* C_MQTTPASSWORD = "...";
+const char* C_MQTTTOPIC = "..."; // Expects 1 for "auf" and 0 for "zu"
+
+#endif
